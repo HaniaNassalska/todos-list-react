003efe453eac2b975a20f15054cdc233f5fde2d0
Hanna Nassalska <hanna.nassalska@gmail.com>
2022-06-23 11:47:13 +0200

    hideDoneTasks renamed to hideDone

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const tasks = [
     { id: 2, content: "Zrobić zadanie", done: true },
 ];
 
-const hideDoneTasks = false;
+const hideDone = false;
 
 function App() {
     return (
@@ -23,8 +23,8 @@ function App() {
 
             <Section
                 title="Lista zadań"
-                extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
-                body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+                extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} />}
+                body={<Tasks tasks={tasks} hideDone={hideDone} />}
             />
         </Container>
     );
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,10 +1,10 @@
 import "./style.css";
 
-const Tasks = ({ tasks, hideDoneTasks }) => (
+const Tasks = ({ tasks, hideDone }) => (
     <ul className="taskList section__listOfTasks">
         {tasks.map(task => (
             <li key={task.id} 
-            className={`taskList__item ${task.done && hideDoneTasks ? " taskList__hiddenTasks" : ""}`}>
+            className={`taskList__item ${task.done && hideDone ? " taskList__hiddenTasks" : ""}`}>
                 <button className={'button__done'}> {task.done ? "✔" : ""}</button>
                 <span className={`${task.done ? "taskList_content--done" : ""}`}  >{task.content}</span>
                 <button className="button__remove">🗑</button>
