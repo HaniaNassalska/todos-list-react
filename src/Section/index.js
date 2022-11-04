3a92a83c4fa8e6f886ffc69d5a05ba60d4d5be60
Hanna Nassalska <hanna.nassalska@gmail.com>
2022-11-04 23:59:27 +0100

    Styled components added in Section comonent

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,14 +1,21 @@
-import "./style.css";
+import { 
+    StyledSection, 
+    StyledSectionHeader, 
+    StyledSectionTitle, 
+    StyledSectionButtons, 
+    StyledSectionDecorationalLine, 
+    StyledSectionListOfTasks 
+} from "./styled";
 
 const Section = ({ title, body, extraHeaderContent }) => (
-    <section className="section">
-        <header className="section__header">
-            <h2 className="section__title">{title}</h2>
-            {extraHeaderContent}
-        </header>
-        <div className="section__decorationLine section__decorationLine--position"></div>
-        {body}
-    </section>
+    <StyledSection>
+        <StyledSectionHeader>
+            <StyledSectionTitle>{title}</StyledSectionTitle>
+            <StyledSectionButtons>{extraHeaderContent}</StyledSectionButtons>
+        </StyledSectionHeader>
+        <StyledSectionDecorationalLine></StyledSectionDecorationalLine>
+        <StyledSectionListOfTasks>{body}</StyledSectionListOfTasks>
+    </StyledSection>
 );
 
 export default Section;
--- src/Section/styled.js
+++ src/Section/styled.js
@@ -1,22 +1,23 @@
-.section {
+import styled from "styled-components"
+
+export const StyledSection = styled.section`
     background-color: white;
     margin-bottom: 1px;
-}
+`
 
-.section__header {
+export const StyledSectionHeader = styled.header`
     display: grid;
     grid-template-columns: 1fr 2fr;
     grid-template-rows: 3;
-}
 
-@media (max-width: 768px) {
-    .section__header {
+
+    @media (max-width: 768px) {
         grid-template-columns: 1fr 200px 1fr;
         grid-template-rows: 4;
     }
-}
+`
 
-.section__title {
+export const StyledSectionTitle = styled.h2`
     padding-left: 20px;
     padding-top: 18px;
     font-size: 20px;
@@ -24,18 +25,16 @@
     grid-column-start: 1;
     grid-column-end: 4;
     grid-row-start: 1;
-}
+`
 
-.section__buttons {
+export const StyledSectionButtons = styled.span`
     padding: 20px;
     grid-column: 3/4;
     grid-row-start: 1;
     justify-content: end;
     align-content: end;
-}
 
-@media (max-width: 768px) {
-    .section__buttons {
+    @media (max-width: 768px) {
         grid-column-start: 2;
         grid-column-end: 3;
         grid-row-start: 2;
@@ -43,20 +42,15 @@
         display: flex;
         flex-wrap: wrap;
     }
-}
-
-.section__decorationLine {
+`
+export const StyledSectionDecorationalLine = styled.div`
     border-bottom: solid 1.5px #e8e8ee;
-}
-
-.section__decorationLine--position {
     grid-column-start: 1;
     grid-column-end: 4;
-}
+`
 
-.section__listOfTasks {
+export const StyledSectionListOfTasks = styled.div`
     grid-row-start: 3;
     grid-column-start: 1;
     grid-column-end: 4;
-}
-
+`
