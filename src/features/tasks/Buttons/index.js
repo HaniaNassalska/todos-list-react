823a111bc3aed3edced43c3237f281ce0853f9f5
Hanna Nassalska <hanna.nassalska@gmail.com>
2022-12-20 14:10:45 +0100

    Wrapper styled component added

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,27 +1,30 @@
-import {Button } from "./styled";
+import { Wrapper, Button } from "./styled";
 import { useSelector, useDispatch } from "react-redux";
 import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
 
 const Buttons = () => {
-    const {tasks, hideDone} = useSelector(selectTasks);
+    const { tasks, hideDone } = useSelector(selectTasks);
     const dispatch = useDispatch();
 
+
     return (
-        tasks.length > 0 && (
-            <div>
-                <Button
-                    onClick={()=> dispatch(toggleHideDone())}
+        <Wrapper>
+            {tasks.length > 0 && (
+                <>
+                    <Button
+                        onClick={() => dispatch(toggleHideDone())}
                     >
-                    {hideDone ? "Pokaż" : "Ukryj"} ukończone
-                </Button>
-                <Button
-                    onClick={()=> dispatch(setAllDone())}
-                    disabled={tasks.every(({ done }) => done)}
-                >
-                    Ukończ wszystkie
-                </Button>
-            </div>
-        )
+                        {hideDone ? "Pokaż" : "Ukryj"} ukończone
+                    </Button>
+                    <Button
+                        onClick={() => dispatch(setAllDone())}
+                        disabled={tasks.every(({ done }) => done)}
+                    >
+                        Ukończ wszystkie
+                    </Button>
+                </>
+            )}
+        </Wrapper>
     );
 }
 
