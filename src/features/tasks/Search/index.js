e5f476daff7f86324313ba5e6b1d5cdc13e4aaf4
Hanna Nassalska <hanna.nassalska@gmail.com>
2022-12-28 17:12:30 +0100

    renamed SearchEngine to Search

---
+++ src/features/tasks/Search/index.js
@@ -0,0 +1,11 @@
+import { StyledForm, Input } from "../Input";
+
+const Search = () => (
+  <StyledForm>
+    <Input
+      placeholder="Filtruj"
+    />
+  </StyledForm>
+);
+
+export default Search;
---
+++ src/features/tasks/StyledButton/index.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  background-color: transparent;
+  border: none;
+  color: ${({ theme }) => theme.colors.teal};
+  transition: 0.3s;
+  padding-top: 12px;
+
+&:hover {
+  filter: brightness(110%);
+  cursor: pointer;
+};
+
+&:disabled {
+  background-color: transparent;
+  border: none;
+  color: ${({ theme }) => theme.colors.grey};
+  cursor: default;
+}
+`
