11cc90de0a57859f2c78938098c8b42661eae9da
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-01-05 21:33:56 +0100

    text changed

--- src/features/author/index.js
+++ src/features/author/index.js
@@ -13,8 +13,8 @@ function AuthorPage() {
       <TextContainer>
         <>
           <p>Cześć!😊</p>
-          <p>Dotychczas zawodowo związana byłam z organizacją pozarządową z branży IT. Początkowo pracowałam przy projektach z Europejskiego Funduszu Społecznego, które moja organizacja kierowana do osób z niepełnosprawnościami, celem nabywania przez nich umiejętności cyfrowych. </p>
-          <p>Przez ostatnie lata zajmowałam się organizacją i zarządzaniem projektami skierowanymi do klientów biznesowych, głównie korporacyjnych.</p>
+          <p>Dotychczas zawodowo związana byłam z organizacją pozarządową z branży IT. Początkowo pracowałam przy projektach z Europejskiego Funduszu Społecznego, które moja organizacja kierowała do osób z niepełnosprawnościami, celem nabywania umiejętności cyfrowych. </p>
+          <p>Przez ostatnie lata zajmowałam się organizacją i zarządzaniem projektami IT skierowanymi do klientów biznesowych, głównie korporacyjnych.</p>
           <p>Zawsze chciałam poszerzyć swoją wiedzę o <em> tajniki </em> programowania, dlatego zapisałam się na kurs. Jeszcze nie wiem, gdzie mnie to zaprowadzi - sama jestem ciekawa. 😎</p>
         </>
       </TextContainer>
